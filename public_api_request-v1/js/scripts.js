b4059590282f0c75a0b19b77c4dc3598156a5084
Shelby Goudy <shelbygoudy@gmail.com>
2020-06-06 08:15:12 -0400

    All 12 cards display

--- public_api_request-v1/js/scripts.js
+++ public_api_request-v1/js/scripts.js
@@ -1,17 +1,7 @@
 const allUsers = 'https://randomuser.me/api/?results=12';
 
 const galleryImg = document.querySelector('#gallery');
-const card = document.createElement('div');
-card.setAttribute('class', 'card');
-galleryImg.appendChild(card);
 
-const cardImgCont = document.createElement('div');
-cardImgCont.setAttribute('class', 'card-img-container');
-card.appendChild(cardImgCont);
-
-const cardInfo = document.createElement('div');
-cardInfo.setAttribute('class', 'card-info-container');
-card.appendChild(cardInfo);
 
 
 // ------------------------------------------
@@ -30,6 +20,19 @@ fetch(allUsers)
 function createProfile (data){
 	console.log(data[0]);
 	data.forEach(each => {
+	
+		const card = document.createElement('div');
+		card.setAttribute('class', 'card');
+		galleryImg.appendChild(card);
+
+		const cardImgCont = document.createElement('div');
+		cardImgCont.setAttribute('class', 'card-img-container');
+		card.appendChild(cardImgCont);
+
+		const cardInfo = document.createElement('div');
+		cardInfo.setAttribute('class', 'card-info-container');
+		card.appendChild(cardInfo);
+
 		cardImgCont.innerHTML = `<img class = "card-img" src = '${each.picture.large}' alt= "profile picture">`;
 		cardInfo.innerHTML = `<h3 id = "name" class = "card-name cap">${each.name.first} ${each.name.last}</h3>
 		<p class = "card-text">${each.email}</p>
