fc4122b07c5cf189a8350c5625c604a30f499de8
Shelby Goudy <shelbygoudy@gmail.com>
2020-06-08 11:11:16 -0400

    modal container displays user on click

--- public_api_request-v1/js/scripts.js
+++ public_api_request-v1/js/scripts.js
@@ -2,7 +2,6 @@ const allUsers = 'https://randomuser.me/api/?results=12&inc=picture,name,email,p
 const galleryImg = document.querySelector('#gallery');
 const body = document.querySelector('body');
 
-
 //Create modal div		
 const modalCont = document.createElement('div');
 modalCont.setAttribute('class', 'modal-container');
@@ -44,6 +43,7 @@ function createProfile (results){
 		<p class = "card-text">${each.email}</p>
 		<p class = "card-text cap" >${each.location.city}</p>`;
 	//  Build the info array
+		const img = `${each.picture.large}`;
 		const fName = `${each.name.first}`;
 		const lName = `${each.name.last}`;
 		const email = `${each.email}`;
@@ -55,12 +55,17 @@ function createProfile (results){
 			const day = dobArr[2].substring(0,2);
 			const year = dobArr[0];
 		const dob = `${month}/${day}/${year}`;
-		const location = `${each.location.street.number} ${each.location.street.name} ${each.location.city}, ${each.location.state}, ${each.location.country} ${each.location.postcode}`;
-		info.push({fName, lName, email, city, phone, dob, location});
+		const location = `${each.location.street.number} ${each.location.street.name} ${each.location.city}, ${each.location.state} ${each.location.postcode}`;
+		info.push({img, fName, lName, email, city, phone, dob, location});
 		})
 	}
 	
 //---------- Return email if card is clicked in any location -------------------//
+/**
+ * [selectGallery description]
+ * @param  {[type]} e [description]
+ * @return {[type]}   [description]
+ */
 function selectGallery(e){
 	const cardData = e.target;
 		if (cardData.tagName === 'P'){
@@ -90,62 +95,49 @@ function selectGallery(e){
 	}
 }
 
+/**
+ * createModal function
+ * @param  {name} user object
+ * @return container information
+ */
 
 function createModal (name){
-	
-	console.log(name)
-	
-}
-
-	// } else {
-	// const nameInfo = wholeCard.querySelector('#name').textContent;
-	// const fNameTarget = nameInfo.split(' ')[0];
-	// const lNameTarget = nameInfo.split(' ')[1];
-	// info.forEach(person => {
-	// 		const first = person.fName;
-	// 		const last = person.lName;
-	// 		const wholeName = first.concat(' ',last);
-	// 		if(nameInfo === wholeName){
-	// 			console.log(nameInfo);
-	// 			console.log(wholeName);
-	// 		}
-	// 	})
-	
-
-	
-	// if(fName === info[0]){
-	// 	console.log(fName)
-	// 	console.log(info[0])
-	// }
-	
-	// 	const modal = document.createElement('div');
-	// 	modal.setAttribute('class', 'modal');
-	// 	modalCont.appendChild(modal);
-	// 	const exitBtn = document.createElement('button');
-	// 	modal.appendChild(exitBtn);
-	// 	exitBtn.setAttribute('type', 'button');
-	// 	exitBtn.setAttribute('id', 'modal-close-btn');
-	// 	exitBtn.setAttribute('class', 'modal-close-btn');
-	// 	exitBtn.innerHTML = '<strong>X</strong>';
+//	Display container when element is clicked	
+	modalCont.hidden = false;
+//	Create modal div and append to modal container	
+	const modal = document.createElement('div');
+	modal.setAttribute('class', 'modal');
+	modalCont.appendChild(modal);
+//	Create Exit Button and append to modal div	
+	const exitBtn = document.createElement('button');
+	modal.appendChild(exitBtn);
+	exitBtn.setAttribute('type', 'button');
+	exitBtn.setAttribute('id', 'modal-close-btn');
+	exitBtn.setAttribute('class', 'modal-close-btn');
+	exitBtn.innerHTML = '<strong>X</strong>';
+//	Create modal info container and append to modal div
+	const infoCont = document.createElement('div');
+	infoCont.setAttribute('class', 'modal-info-container');
+	modal.appendChild(infoCont);
+//	Choose info to display in container upon click
+		info.forEach(each => {
+			if (each.email === name){
+				infoCont.innerHTML = `
+					<img class="modal-img" src="${each.img}" alt="profile picture">
+					<h3 id="name" class="modal-name cap">${each.fName} ${each.lName}</h3>
+			        <p class="modal-text">${each.email}</p>
+			        <p class="modal-text cap">${each.city}</p>
+			        <hr>
+			        <p class="modal-text">${each.phone}</p>
+			        <p class="modal-text">${each.location}</p>
+			        <p class="modal-text">Birthday: ${each.dob}</p>`;			
+			}
+		})	
+	}	
 		
-	// 	const infoCont = document.createElement('div');
-	// 	infoCont.setAttribute('class', 'modal-info-container');
-	// 	modal.appendChild(infoCont);
+		
 
-	// 	infoCont.innerHTML = `
-	// 	<img class = "modal-img" src = '${each.picture.large}' alt = "profile picture">
-	// 	<h3 id="name" class="modal-name cap">${each.name.first} ${each.name.last}</h3>
- //        <p class="modal-text">${each.email}</p>
- //        <p class="modal-text cap">${each.location.city}</p>
- //        <hr>
- //        <p class="modal-text">${each.phone}</p>
- //        <p class="modal-text">${each.location}</p>
- //        <p class="modal-text">'Birthday' ${each.dob}</p>`;
-
-
-
-
-
+		
 
 
 // ------------------------------------------
